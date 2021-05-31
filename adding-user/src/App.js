5def6f516dcbec162b19fb3ed7337c8a1914f614
rutwikhdev <rutwikhdev@gmail.com>
2021-05-31 19:14:59 +0530

    app 1

---
+++ adding-user/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
+    });
+  }
+
+  return (
+    <div className="App">
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList}/>
+    </div>
+  );
+}
+
+export default App;
